8ef99a6a2edcb4adb816f82f06491652ff0de3c2
Sebastian Risty <sristy@uiowa.edu>
2022-12-02 15:38:59 -0600

    implemented client and moved mainGUI driver into it

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.Formatter;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-class Client {
+class Client extends Application implements Runnable{
     private final String ip;
     private final int port;
-    private BufferedReader in;
-    private PrintWriter out;
+    private Scanner input; // input from server
+    private Formatter output; // output to server
     private Socket serverSocket;
+    private Controller controller;
 
     private enum messages{
         LOGIN_FAILED,
@@ -21,20 +32,33 @@ class Client {
     public Client(String ip, String port) {
         this.ip = ip;
         this.port = Integer.parseInt(port);
+        Executors.newFixedThreadPool(1).execute(this); // add thread for client
     }
 
     public static void main(String[] args) throws UnknownHostException {
         Client client;
         switch (args.length) {
-            case 2:
-                client = new Client(args[0], args[1]);
+            case 3:
+                if(args[0].equals("text")){
+                    //TODO: call text class
+                } else {
+                    launch();
+                }
+                client = new Client(args[1], args[2]);
                 break;
             case 1:
-                client = new Client(args[0], "23704");
+                if(args[0].equals("text")){
+                    //TODO: call text class
+                } else {
+                    launch();
+                }
+                client = new Client(InetAddress.getLocalHost().getHostAddress(), "23704");
                 break;
             default:
+                launch();
                 client = new Client(InetAddress.getLocalHost().getHostAddress(), "23704");
         }
+
         client.startClient();
     }
 
@@ -42,7 +66,6 @@ class Client {
         try {
             connectToServer();
             openConnection();
-            game();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,51 +73,48 @@ class Client {
 
     private void connectToServer() {
         try {
-            serverSocket = new Socket(InetAddress.getByName("127.0.0.1"), 23704);
+            serverSocket = new Socket(ip, port);
         } catch (IOException ioException) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            connectToServer();
+            connectToServer(); //TODO: limit retry count
         }
     }
 
-    private void openConnection() throws IOException {
-        out = new PrintWriter(
-                new OutputStreamWriter(
-                        serverSocket.getOutputStream()));
-        in = new BufferedReader(
-                new InputStreamReader(
-                        serverSocket.getInputStream()));
+    private void openConnection() throws IOException { // TODO conform to server std
+        input = new Scanner(serverSocket.getInputStream());
+        output = new Formatter(serverSocket.getOutputStream());
     }
 
-    private void game() throws IOException {
-        init();
-        while (true) {
-            Scanner scnr = new Scanner(System.in);
-            String message = null;
-            message = scnr.nextLine();
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL fxmlFile = getClass().getResource("LoginFXMl.fxml");
 
-            out.println(message.toLowerCase());
-            out.flush();
+        controller = new Controller();
+        controller.setClient(this);
 
-            String serverInput;
-            while ((serverInput = in.readLine()) != null) {
-                out.println(serverInput);
-                out.flush();
-            }
+        assert fxmlFile != null;
+        Parent root = FXMLLoader.load(fxmlFile);
 
-        }
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Word Game");
+        primaryStage.setScene(scene);
+
+        controller.setRoot(root);
+        //controller.setScene(scene);
+
+        primaryStage.show();
     }
 
-    private void init() throws IOException {
-        String startMessage = null;
-        while (startMessage == null) {
-            startMessage = in.readLine();
-        }
+    @Override
+    public void run(){
+        System.out.println("AWAITING SERVER DATA");
+        while(true){
+            // TODO: handle server messages to update gui here
 
+        }
     }
 }
-
