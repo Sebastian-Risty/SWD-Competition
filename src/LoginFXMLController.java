31102938dfd1151752fee054f0a54a35342a24c8
sloecke <sloecke@uiowa.edu>
2022-12-01 19:55:05 -0600

    Added jfoenix library

--- src/LoginFXMLController.java
+++ src/LoginFXMLController.java
@@ -1,20 +1,37 @@
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRippler;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class LoginFXMLController {
 
     @FXML
     private JFXHamburger hamburger;
+    @FXML
+    private JFXRippler verifyRippler;
 
     @FXML
     private JFXPasswordField passwordField;
 
     @FXML
     private JFXTextField usernameField;
+    @FXML
+    private Pane pane;
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private GridPane gridPane;
 
+    public void initialize() {
+        verifyRippler = new JFXRippler(pane);
+
+        gridPane.getChildren().add(verifyRippler);
+    }
     @FXML
     void signUpButtonListener(ActionEvent event) {
         // switch to sign up screen
@@ -32,7 +49,8 @@ public class LoginFXMLController {
             // go to the home page of an account
         }
         else {
-            // set stuff to let the user know it was wrong and try again
+
+            verifyRippler.createManualRipple();
         }
     }
 
