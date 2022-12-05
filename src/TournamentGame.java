1bef0381dc467038e7e1066f2cc7d479b8ecbd1d
sloecke <sloecke@uiowa.edu>
2022-12-05 03:46:07 -0600

    JavaDocs

--- src/TournamentGame.java
+++ src/TournamentGame.java
@@ -1,25 +1,49 @@
 import java.io.File;
 
+/**
+ * TournamentGame class that extends OneVsOne and defines a tournament game
+ * @see OneVsOne
+ */
 public class TournamentGame extends OneVsOne {
-
+    /**
+     * Tournament name
+     */
     String tournamentName;
-
+    /**
+     * Getter for tournament name
+     * @return tournament name
+     */
     public String getTournamentName() {
         return tournamentName;
     }
-
+    /**
+     * TournamentGame constructor
+     * @param matchTime the match time
+     * @param filePath the path to the word file
+     * @param fileIndex the index in the word file
+     * @param tournamentName the name of the tournament
+     *
+     */
     public TournamentGame(int matchTime, File filePath, int fileIndex, String tournamentName) {
         super(matchTime, filePath, fileIndex);
         setGamemode("Tournament");
         this.tournamentName = tournamentName;
     }
 
+    /**
+     * TournamentGame constructor with two inputs
+     * @param matchTime the match time
+     * @param tournamentName the name of the tournament
+     */
     public TournamentGame(int matchTime, String tournamentName) {
         super(matchTime);
         setGamemode("Tournament");
         this.tournamentName = tournamentName;
     }
-
+    /**
+     * pregameLobby method that waits for two players to connect. Overrides the OneVsOne version
+     * @throws InterruptedException exception
+     */
     @Override
     public void pregameLobby() throws InterruptedException {
         while (!isInProgress()) {
@@ -32,7 +56,9 @@ public class TournamentGame extends OneVsOne {
             }
         }
     }
-
+    /**
+     * startGame method for starting the match timer and keeping track of the match time
+     */
     @Override
     public void startGame() {
         if (!isFinished()) {
