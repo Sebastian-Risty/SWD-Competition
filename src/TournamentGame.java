f1db89130eeef9f35abe5d8c6b33c8872a9a7a52
ColeArduser <carduser@uiowa.edu>
2022-12-04 18:51:59 -0600

    SAVEPOINT

---
+++ src/TournamentGame.java
@@ -0,0 +1,40 @@
+import java.io.File;
+
+public class TournamentGame extends Game {
+
+    public TournamentGame(int matchTime, File filePath, int fileIndex) {
+        super(matchTime, filePath, fileIndex);
+        setGamemode("OneVsOne");
+    }
+
+    public TournamentGame(int matchTime) {
+        super(matchTime);
+        setGamemode("OneVsOne");
+    }
+
+    @Override
+    public void pregameLobby() throws InterruptedException {
+        System.out.println("IN 1v1 PREGAME");
+        while (!isInProgress()) {
+            Thread.sleep(10);
+            if (getNumConnectedClients() == 2) {
+                System.out.println("CHANGED FLAG");
+                changeProgressFlag();
+                changeStartFlag();
+                startGame();
+            }
+        }
+    }
+
+    @Override
+    public void startGame() {
+        if (!isFinished()) {
+            System.out.println("STARTING MATCH TIMER");
+            long startTime = System.currentTimeMillis();
+            while (((System.currentTimeMillis() - startTime) / 1000) < getMatchTime()) ;
+            System.out.println("MATCH ENDED");
+            changeEndFlag();
+        }
+    }
+
+}
