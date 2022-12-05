931d31250649775f26e9fb679cfd336ea57129a3
sloecke <sloecke@uiowa.edu>
2022-12-05 04:03:28 -0600

    fixed rippler

--- src/LoginScreenController.java
+++ src/LoginScreenController.java
@@ -11,7 +11,6 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-
 import java.io.IOException;
 
 /**
