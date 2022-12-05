618b4f06893dec3fb10e551d2c931d05504b099c
Sebastian Risty <sristy@uiowa.edu>
2022-12-05 01:25:23 -0600

    1v1 + BR dox

--- src/OneVsOne.java
+++ src/OneVsOne.java
@@ -1,17 +1,36 @@
 import java.io.File;
 
+/**
+ * Represents specific rules for head on head matches
+ */
 public class OneVsOne extends Game {
 
+    /**
+     * OneVsOne constructor
+     * @param matchTime How long each match should last
+     * @param filePath File object path
+     * @param fileIndex Index to read from file
+     *                  Calls super constructor
+     */
     public OneVsOne(int matchTime, File filePath, int fileIndex) {
         super(matchTime, filePath, fileIndex);
         setGamemode("OneVsOne");
     }
 
+    /**
+     * OneVsOne constructor
+     * @param matchTime How long the match should be
+     *                  Calls super constructor
+     */
     public OneVsOne(int matchTime) {
         super(matchTime);
         setGamemode("OneVsOne");
     }
 
+    /**
+     * Starts the match once 2 clients are connected
+     * @throws InterruptedException If thread is interrupted
+     */
     @Override
     public void pregameLobby() throws InterruptedException {
         while (!isInProgress()) {
@@ -24,6 +43,9 @@ public class OneVsOne extends Game {
         }
     }
 
+    /**
+     * Waits for match time then finished the match
+     */
     @Override
     public void startGame() {
         if (!isFinished()) {
