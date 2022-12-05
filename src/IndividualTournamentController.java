49122c801741ff0bca988eecc59df428c5d610a3
sloecke <sloecke@uiowa.edu>
2022-12-04 23:56:05 -0600

    working on tournament controller

--- src/IndividualTournamentController.java
+++ src/IndividualTournamentController.java
@@ -26,34 +26,26 @@ public class IndividualTournamentController extends Controller {
 
         startButton.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
         mainMenuButton.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
-        int j = 0;
-        while(j<getTournamentData().length - 2) {
 
+        int j = 2;
+        while(j<getTournamentData().length - 2) {
+            if(getTournamentData()[j].equals(getPlayer().getUsername())) {
+                addToUserPane(String.valueOf(((j-1)/3)+1), getTournamentData()[j], getTournamentData()[j+1], getTournamentData()[j+2]);
+            }
+            j+=3;
         }
 
         boolean length = getTournamentData().length >= 17;
-        System.out.println(Arrays.toString(getTournamentData()));
-
         if(length) {
             for(int i = 2; i<15; i+=3) {
-                if(getTournamentData()[i].equals(getPlayer().getUsername())) {
-                    addToUserPane(String.valueOf(((i-1)/3)+1), getTournamentData()[i], getTournamentData()[i+1], getTournamentData()[i+2]);
-                }
                 addToLeaderBoardPane(String.valueOf(((i-1)/3)), getTournamentData()[i], getTournamentData()[i+1], getTournamentData()[i+2]);
             }
         }
         else {
             for(int i = 2; i<getTournamentData().length-2; i+=3) {
-                System.out.println(i);
-                if(getTournamentData()[i].equals(getPlayer().getUsername())) {
-                    addToUserPane(String.valueOf(((i-1)/3)+1), getTournamentData()[i], getTournamentData()[i+1], getTournamentData()[i+2]);
-                }
                 addToLeaderBoardPane(String.valueOf(((i-1)/3)+1), getTournamentData()[i], getTournamentData()[i+1], getTournamentData()[i+2]);
             }
         }
-
-        // username, wins, gamesleft
-
     }
 
     @FXML
